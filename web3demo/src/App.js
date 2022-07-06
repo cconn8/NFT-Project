49b7ec633865b824dc6b586449d9257ffe37bcb6
Colm Conneely <colm.conneely8@mail.dcu.ie>
2022-07-06 09:03:59 +0100

    updated app.js to include more buttons (Connect Device, Connect Wallet, Enter Details, Start/Upload

--- web3demo/src/App.js
+++ web3demo/src/App.js
@@ -9,9 +9,29 @@ function App() {
       <div className='container'>
         <div className='row'>
           <form class="gradient col-lg-5 mt-5" style={{borderRadius:"25px", boxShadow:"1px 1px 15px #000000"}}>
-            <h4 style={{color: "#FFFFFF"}}>Mint Portal</h4>
-            <h5 style={{color: "#FFFFFF"}}>Please connect your wallet</h5>
-            <Button style={{marginBottom:"5px"}}>Connect Wallet</Button>
+            <h4 style={{color: "#FFFFFF"}}>IoT-NFT App</h4>
+
+            <section>
+              <Button style={{marginBottom:"5px"}}>Connect Device</Button><Button style={{marginBottom:"5px"}}>Connect Wallet</Button>
+            </section>
+
+            <form>
+              <h5 style={{color: "#FFFFFF"}}>Enter Details</h5>
+              <label>Data Owner</label><br></br>
+              <input type="text"></input><br></br>
+
+              <label>Description</label><br></br>
+              <input type="text"></input><br></br>
+
+              <label>Upload Interval seconds</label><br></br>
+              <input type="text"></input><br></br>
+
+              <Button style={{marginBottom:"5px"}}>Submit Details</Button>
+            </form>
+
+            <section>
+              <Button style={{marginBottom:"5px"}}>Start/Upload</Button>
+            </section>
             <div class="card" id="wallet-address" style={{marginTop:"3px", boxShadow:"1px 1px 4px #000000"}}>
               <label style={{color: "#FFFFFF"}} for="floatingInput">Wallet Address</label>
               <input type="number" name="amount" defaultValue="1" minimum="1" maximum="5"/>
